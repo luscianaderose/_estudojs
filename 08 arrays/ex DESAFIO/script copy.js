88fff33fa50bd288dc68f06a86a3ff36ca5ecfc0
luscianaderose <luscianaderose@gmail.com>
2025-04-02 11:07:51 -0300

    aula 8 desafio

--- 08 arrays/ex DESAFIO/script copy.js
+++ 08 arrays/ex DESAFIO/script copy.js
@@ -44,10 +44,10 @@
 // Exemplo de entrada e saída
 
 const produtos = [
-    { nome: "Arroz", categoria: "alimentos", preco: 20, popularidade: 500 },
+    { nome: "arroz", categoria: "alimentos", preco: 20, popularidade: 500 },
     { nome: "Sabonete", categoria: "higiene", preco: 5, popularidade: 300 },
     { nome: "Refrigerante", categoria: "bebidas", preco: 8, popularidade: 800 },
-    { nome: "Feijão", categoria: "alimentos", preco: 12, popularidade: 600 },
+    { nome: "feijão", categoria: "alimentos", preco: 12, popularidade: 600 },
     { nome: "Cerveja", categoria: "bebidas", preco: 10, popularidade: 1000 }
 ]
 
