de743d1bed3a4978fa7a49363150c323409d2d58
luscianaderose <luscianaderose@gmail.com>
2025-10-06 09:33:29 -0300

    aula 20 promisse

--- 20 Promises e Async Await/ex04/script.js
+++ 20 Promises e Async Await/ex04/script.js
@@ -1,2 +1,13 @@
-// 4. Crie uma função que retorna uma Promise que pode ser resolvida ou rejeitada aleatoriamente.
-// Trate o erro com .catch()
+// 4. Crie uma função que retorna uma Promise que pode ser resolvida 
+// ou rejeitada aleatoriamente. Trate o erro com .catch()
+
+function criarPromessa(){
+    return new Promise((_, reject) => {}
+
+    )
+}
+
+const promessa = criarPromessa()
+
+promessa
+.catch()
--- 20 Promises e Async Await/ex06/script.js
+++ 20 Promises e Async Await/ex06/script.js
@@ -1,2 +1,15 @@
-// 6. Faça uma requisição para a API pública https://jsonplaceholder.typicode.com/users e exiba
-// apenas o nome (name) e o email (email) de cada usuário.
+// 6. Faça uma requisição para a API pública https://jsonplaceholder.typicode.com/users 
+// e exiba apenas o nome (name) e o email (email) de cada usuário.
+
+// const dados = (dados => dados.filter(dados => dados.name))
+
+fetch('https://jsonplaceholder.typicode.com/users')
+    .then(response => response.json())
+    .then(dados => {
+        const usuarios = dados.map(user => ({
+            name: user.name,
+            email: user.email
+        }))
+        console.log(usuarios)
+    })
+    .catch(error => console.error('Erro', error))
